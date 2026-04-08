e6bbad557752f9510881c35984f1a22a354686d3
serraguti <pacoserranox@gmail.com>
2026-04-08 10:18:53 +0200

    uppp

---
+++ MvcOAuthApiEmpleados/Controllers/ManagedController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using MvcOAuthApiEmpleados.Models;
+using MvcOAuthApiEmpleados.Services;
+using System.Security.Claims;
+
+namespace MvcOAuthApiEmpleados.Controllers
+{
+    public class ManagedController : Controller
+    {
+        private ServiceEmpleados service;
+
+        public ManagedController(ServiceEmpleados service)
+        {
+            this.service = service;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            string token = await
+                this.service.LogInAsync(model.UserName, model.Password);
+            if (token == null)
+            {
+                ViewData["MENSAJE"] = "Credenciales incorrectas";
+                return View();
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Ya tienes tu Token!!!";
+                HttpContext.Session.SetString("TOKEN", token);
+                ClaimsIdentity identity =
+                    new ClaimsIdentity
+                    (CookieAuthenticationDefaults.AuthenticationScheme
+                    , ClaimTypes.Name, ClaimTypes.Role);
+                //ALMACENAMOS EL NOMBRE DEL USUARIO (BONITO)
+                identity.AddClaim(new Claim(ClaimTypes.Name, model.UserName));
+                //ALMACENAMOS EL PASSWORD DEL USUARIO COMO IDENTIFIER
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier
+                    , model.Password));
+                ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+                //DAMOS DE ALTA AL USUARIO DURANTE 20 MINUTOS
+                await HttpContext.SignInAsync
+                    (CookieAuthenticationDefaults.AuthenticationScheme,
+                    principal, new AuthenticationProperties
+                    {
+                        ExpiresUtc = DateTime.UtcNow.AddMinutes(20)
+                    });
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync
+                (CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
